eabdac3d220695d0061d213a3dd369b316698651
ryanzola <ryanzola@me.com>
2018-06-28 21:32:21 -0400

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,50 @@
+import { loadLevel } from './loaders.js';
+import { loadBackgroundSprites, loadMarioSprite } from './sprites.js';
+import { createBackgroundLayer } from './layers.js';
+import Compositor from './Compositor.js';
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+function createSpriteLayer(sprite, pos) {
+  return function drawSpriteLayer(context) {
+    sprite.draw('idle', context, pos.x, pos.y);
+  }
+}
+
+Promise.all([
+  loadMarioSprite(),
+  loadBackgroundSprites(),
+  loadLevel('1-1')
+])
+.then(
+  ([marioSprite, backgroundSprites, level]) => {
+    const comp = new Compositor();
+
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
+    comp.layers.push(backgroundLayer);
+
+    const pos = {
+      x: 64,
+      y: 64
+    }
+    
+    const vel = {
+      x: 2,
+      y: -10
+    }
+
+    const spriteLayer = createSpriteLayer(marioSprite, pos);
+    comp.layers.push(spriteLayer);
+
+    function update() {
+      comp.draw(context);
+      pos.x += vel.x;
+      pos.y += vel.y;
+
+      requestAnimationFrame(update);
+    }
+
+    update();
+  }
+);
