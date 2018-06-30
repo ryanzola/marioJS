451676bfca7614f5e5bcde90f342ec4985be6081
ryanzola <ryanzola@me.com>
2018-06-30 15:58:55 -0400

    super cool collision detection, debug layer, refactored keyboard input

--- public/js/main.js
+++ public/js/main.js
@@ -1,41 +1,33 @@
-import Keyboard from './KeyboardState.js';
 import Timer from './Timer.js';
 import { loadLevel } from './loaders.js';
 import { createMario } from './entities.js';
+import { createCollisionLayer } from './layers.js';
+import { setupKeyboard } from './input.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
-  reateMario(),
+  createMario(),
   loadLevel('1-1')
 ])
 .then(([
   mario,
   level
 ]) => {
-  const gravity = 2000;
   mario.pos.set(64, 180);
+
+  level.comp.layers.push(createCollisionLayer(level));
 
   level.entities.add(mario);
 
-  const SPACE = 32;
-  const input = new Keyboard();
-  input.addMapping(SPACE, keyState => {
-    if (keyState) {
-      mario.jump.start();
-    } else {
-      mario.jump.cancel();
-    }
-  });
+  const input = setupKeyboard(mario);
   input.listenTo(window);
 
   const timer = new Timer();
-
   timer.update = function update(deltaTime) {
-    mario.update(deltaTime);
+    level.update(deltaTime);
     level.comp.draw(context);
-    mario.vel.y += gravity * deltaTime;
   };
 
   timer.start();
