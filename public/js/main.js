5486a29fa970c8da145f62ebfe974638d5364223
ryanzola <ryanzola@me.com>
2018-07-14 11:54:08 -0400

    No more hard-coded entities. All are coming from a json file!

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,22 @@
 import Camera from './Camera.js';
 import Timer from './Timer.js';
-import { loadLevel } from './loaders/level.js';
+import { createLevelLoader } from './loaders/level.js';
 import { loadEntities } from './entities.js';
 import { setupKeyboard } from './input.js';
 
+async function main(canvas) {
+  const context = canvas.getContext('2d');
 
-const canvas = document.getElementById('screen');
-const context = canvas.getContext('2d');
+  const entityFactory = await loadEntities();
+  const loadLevel = await createLevelLoader(entityFactory);
+  const level = await loadLevel('1-1');
 
-Promise.all([
-  loadEntities(),
-  loadLevel('1-1')
-])
-.then(([ entity, level ]) => {
   const camera = new Camera();
-  window.camera = camera;
 
-  const mario = entity.mario();
+  const mario = entityFactory.mario();
   mario.pos.set(64, 180);
 
-  const goomba = entity.goomba();
-  goomba.pos.set(220, 100);
-
-  const koopa = entity.koopa();
-  koopa.pos.set(260, 100);
-
   level.entities.add(mario);
-  level.entities.add(goomba);
-  level.entities.add(koopa);
 
   const input = setupKeyboard(mario);
   input.listenTo(window);
@@ -36,7 +25,7 @@ Promise.all([
   timer.update = function update(deltaTime) {
     level.update(deltaTime);
 
-    if(mario.pos.x > 100) {
+    if (mario.pos.x > 100) {
       camera.pos.x = mario.pos.x - 100;
     }
 
@@ -44,4 +33,7 @@ Promise.all([
   };
 
   timer.start();
-});
+}
+
+  const canvas = document.getElementById('screen');
+  main(canvas);
